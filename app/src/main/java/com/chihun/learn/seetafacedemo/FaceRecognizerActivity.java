78e5b173124fea1af9ee1dd868d6e8ff09364dee
zhangjunhui <zhangjunhui@cvte.com>
2019-09-10 11:25:57 +0800

    initialize commit

---
+++ app/src/main/java/com/chihun/learn/seetafacedemo/FaceRecognizerActivity.java
@@ -0,0 +1,106 @@
+package com.chihun.learn.seetafacedemo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.cvte.vslam.seetalib.seeta.FaceRecognizer;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class FaceRecognizerActivity extends AppCompatActivity {
+
+    private static final String TAG = FaceRecognizerActivity.class.getSimpleName();
+
+    private CameraBridgeViewBase cameraBridgeViewBase;
+    private Mat mRgba;
+    private Mat mGray;
+
+    private CameraBridgeViewBase.CvCameraViewListener2 mCvCameraViewListener2 = new CameraBridgeViewBase.CvCameraViewListener2() {
+
+        private FaceRecognizer mFaceRecognizer = FaceRecognizer.getInstance();
+
+        @Override
+        public void onCameraViewStarted(int width, int height) {
+            Log.d(TAG, "onCameraViewStarted()");
+            mRgba = new Mat(height, width, CvType.CV_8UC4);
+            mGray = new Mat(height, width, CvType.CV_8UC1);
+            //在这里调用所有需要提前初始化的native方法
+            mFaceRecognizer.loadEngine();
+            mFaceRecognizer.registerFace();
+        }
+
+        @Override
+        public void onCameraViewStopped() {
+            mRgba.release();
+            mGray.release();
+
+            //在这里析构和释放所有之前初始化和分配内存的native方法
+            mFaceRecognizer.releaseEngine();
+        }
+
+        @Override
+        public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            mRgba = inputFrame.rgba();
+            mGray = inputFrame.gray();
+
+            //在这里调用处理每一张frame的native方法 记得在方法中传入的是long型的
+            mFaceRecognizer.recognize(mRgba.getNativeObjAddr());
+            return mRgba;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_face_recognizer);
+
+        initView();
+    }
+
+    private void initView() {
+        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.CameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(mCvCameraViewListener2);
+
+        //在这里设置图像的大小，在手机中图像需要横屏状态，图片太大的话会卡顿
+        cameraBridgeViewBase.setMaxFrameSize(640, 480);
+
+        cameraBridgeViewBase.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                finish();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.enableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        disableCamera();
+    }
+
+    public void disableCamera() {
+        if (cameraBridgeViewBase != null)
+            cameraBridgeViewBase.disableView();
+    }
+}
---
+++ seetalib/src/main/cpp/jni/facerecognize.cpp
@@ -0,0 +1,141 @@
+#include <jni.h>
+#include <string>
+#include <vector>
+#include <android/log.h>
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include <Seeta/FaceEngine.h>
+#include <Seeta/Struct_cv.h>
+#include <Seeta/Struct.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <array>
+#include <map>
+#include <iostream>
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG , "Seeta", __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN , "Seeta", __VA_ARGS__)
+
+static seeta::FaceEngine *FE = NULL;
+static std::map<int64_t, std::string> GalleryIndexMap;
+// recognization threshold
+static float threshold = 0.1;
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_cvte_vslam_seetalib_seeta_FaceRecognizer_initNativeEngine(JNIEnv *env, jobject instance,
+                                                              jstring detectModelFile_
+                                                              , jstring markerModelFile_
+                                                              , jstring recognizeModelFile_) {
+    const char *detectModelFile = env->GetStringUTFChars(detectModelFile_, 0);
+    const char *markerModelFile = env->GetStringUTFChars(markerModelFile_, 0);
+    const char *recognizeModelFile = env->GetStringUTFChars(recognizeModelFile_, 0);
+
+    seeta::ModelSetting::Device device = seeta::ModelSetting::AUTO;
+    int id = 0;
+    seeta::ModelSetting FD_model(detectModelFile, device, id );
+    seeta::ModelSetting PD_model(markerModelFile, device, id );
+    seeta::ModelSetting FR_model(recognizeModelFile, device, id );
+    FE = new seeta::FaceEngine(FD_model, PD_model, FR_model, 2, 16 );
+
+    //set face detector's min face size
+    FE->FD.set(seeta::FaceDetector::PROPERTY_MIN_FACE_SIZE, 80 );
+    FE->FD.set(seeta::FaceDetector::PROPERTY_VIDEO_STABLE, 1);
+    //set face detect threshold
+    FE->FD.set(seeta::FaceDetector::PROPERTY_THRESHOLD1, 0.60f);
+
+    int res = EXIT_SUCCESS;
+    env->ReleaseStringUTFChars(detectModelFile_, detectModelFile);
+    env->ReleaseStringUTFChars(markerModelFile_, markerModelFile);
+    env->ReleaseStringUTFChars(recognizeModelFile_, recognizeModelFile);
+    return (jint)res;
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_cvte_vslam_seetalib_seeta_FaceRecognizer_nativeRegisterFace(JNIEnv *env, jobject instance, jobject faceList) {
+
+    if(NULL == FE) {
+        LOGW("FE is NULL");
+        return;
+    }
+
+    jclass jArrayList = env->GetObjectClass(faceList);
+    jmethodID jArrayList_get = env->GetMethodID(jArrayList, "get", "(I)Ljava/lang/Object;");
+    jmethodID jArrayList_size = env->GetMethodID(jArrayList, "size", "()I");
+    jint len = env->CallIntMethod(faceList, jArrayList_size);
+    LOGD("face len: %d", len);
+    GalleryIndexMap.clear();
+    for (int i = 0; i < len; i++) {
+        jstring filepath_ = (jstring) env->CallObjectMethod(faceList, jArrayList_get, i);
+        const char *filepath = env->GetStringUTFChars(filepath_, 0);
+        LOGD("filepath: %s", filepath);
+
+        seeta::cv::ImageData image = cv::imread( filepath );
+        auto id = FE->Register( image );
+        LOGD("Registered id = %lld", id);
+        if(id >= 0) {
+            GalleryIndexMap.insert( std::make_pair( id, filepath ) );
+        }
+        env->ReleaseStringUTFChars(filepath_, filepath);
+    }
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_cvte_vslam_seetalib_seeta_FaceRecognizer_nativeRecognition(JNIEnv *env, jobject instance, jlong addr) {
+
+    if(NULL == FE) {
+        LOGW("FE is NULL");
+        return;
+    }
+    // TODO
+    cv::Mat &frame = *(cv::Mat *) addr;
+    cv::Mat rgb_img;
+    cv::cvtColor(frame, rgb_img, cv::COLOR_RGBA2BGR);
+
+    seeta::cv::ImageData image = rgb_img;
+
+    // Detect all faces
+    std::vector<SeetaFaceInfo> faces = FE->DetectFaces( image );
+
+    for( SeetaFaceInfo &face : faces )
+    {
+        // Query top 1
+        int64_t index = -1;
+        float similarity = 0;
+
+        auto points = FE->DetectPoints(image, face);
+
+        auto queried = FE->QueryTop( image, points.data(), 1, &index, &similarity );
+
+        cv::rectangle( frame, cv::Rect( face.pos.x, face.pos.y, face.pos.width, face.pos.height ), CV_RGB( 128, 128, 255 ), 3 );
+        for (int i = 0; i < 5; ++i)
+        {
+            auto &point = points[i];
+            cv::circle(frame, cv::Point(point.x, point.y), 2, CV_RGB(128, 255, 128), -1);
+        }
+
+        // no face queried from database
+        if (queried < 1) continue;
+
+        // similarity greater than threshold, means recognized
+        LOGW("similarity: %f", similarity);
+        if( similarity > threshold )
+        {
+            std::string name = GalleryIndexMap[index];
+            LOGD("name: %s", name.c_str());
+            cv::putText( frame, name, cv::Point( face.pos.x, face.pos.y - 5 ), CV_FONT_HERSHEY_COMPLEX, 1, CV_RGB( 255, 128, 128 ) );
+        }
+    }
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_cvte_vslam_seetalib_seeta_FaceRecognizer_releaseNativeEngine(JNIEnv *env, jobject instance) {
+    if(NULL != FE) {
+        delete FE;
+    }
+    int ret = EXIT_SUCCESS;
+    return (jint)ret;
+}
